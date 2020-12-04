824f37d1ea2e4991634c617946b9d05549f7dde3
migueldeoleiros <migueldeoleiros@gmail.com>
2020-12-04 12:13:14 +0100

    changes on scripts repo

--- .scripts/capsLock2Esc.sh
+++ .scripts/capsLock2Esc.sh
@@ -6,3 +6,6 @@
 #It makes easier the use of vim modes
 
 setxkbmap -option caps:escape
+setxkbmap -option 'shift:both_capslock_cancel' # when both Shift buttons are pressed simultaneously, they turn Caps Lock on. it can be switched off by pressing Shift
+
+xmodmap ~/.Xmodmap
---
+++ .scripts/voidInstall.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+xbps-install -S alacritty 
+xbps-install -S neovim
+xbps-install -S qutebrowser
+xbps-install -S ranger 
+xbps-install -S cmake
+xbps-install -S gnugp
+xbps-install -S pass
+xbps-install -S discord 
+xbps-install -S telegram-desktop
+xbps-install -S firefox
+xbps-install -S feh
+xbps-install -S deepin-screenshot
+xbps-install -S picom
+xbps-install -S octave
+xbps-install -S nvidia
+xbps-install -S htop
+xbps-install -S ytop
+xbps-install -S zathura
+xbps-install -S gimp
